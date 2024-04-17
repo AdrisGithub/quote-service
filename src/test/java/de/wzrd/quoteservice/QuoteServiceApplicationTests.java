bdda4070bb90ca09b5adb865469594b21f8e1506
Adrian John <adrianjohn6543@gmail.com>
2024-04-17 20:41:32 +0200

    add: example test

--- src/test/java/de/wzrd/quoteservice/QuoteServiceApplicationTests.java
+++ src/test/java/de/wzrd/quoteservice/QuoteServiceApplicationTests.java
@@ -1,15 +1,24 @@
 package de.wzrd.quoteservice;
 
+import de.wzrd.quoteservice.quotes.control.QuoteRepository;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Collections;
 
 @SpringBootTest
 class QuoteServiceApplicationTests {
+    @Mock
+    private QuoteRepository repository;
 
-	@Test
-	void contextLoads() {
+    @Test
+    void contextLoads() {
         Assertions.assertTrue(true);
-	}
+        Mockito.when(repository.findAll()).thenReturn(Collections.emptyList());
+        Assertions.assertEquals(Collections.emptyList(), repository.findAll());
+    }
 
 }
