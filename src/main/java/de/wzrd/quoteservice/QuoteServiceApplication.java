697181b655e9d737f893d82e2169c5a972fbb9dd
Adrian John <adrianjohn6543@gmail.com>
2024-04-18 21:55:15 +0200

    refactor: Application

--- src/main/java/de/wzrd/quoteservice/QuoteServiceApplication.java
+++ src/main/java/de/wzrd/quoteservice/QuoteServiceApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class QuoteServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(QuoteServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(QuoteServiceApplication.class, args);
+    }
 
 }
