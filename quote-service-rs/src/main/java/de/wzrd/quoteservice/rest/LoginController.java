db45bd882a8a8afef11b5fdb4b6f0d8aeb0a62a5
Adrian John <adrianjohn6543@gmail.com>
2024-04-28 19:49:44 +0200

    add: a get method

--- quote-service-rs/src/main/java/de/wzrd/quoteservice/rest/LoginController.java
+++ quote-service-rs/src/main/java/de/wzrd/quoteservice/rest/LoginController.java
@@ -1,19 +1,53 @@
 package de.wzrd.quoteservice.rest;
 
+import de.wzrd.quoteservice.gen.GetUser;
+import de.wzrd.quoteservice.gen.LoginUser;
+import de.wzrd.quoteservice.gen.SessionApi;
 import lombok.Getter;
-import org.springframework.stereotype.Component;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.annotation.SessionScope;
 
+import java.time.OffsetDateTime;
+import java.util.Objects;
 import java.util.UUID;
 
 @Getter
 @SessionScope
-@Component
-public class LoginController {
+@RequiredArgsConstructor
+@RestController
+public class LoginController implements SessionApi {
 
-    private final UUID id;
+    private UUID id;
 
-    public LoginController() {
-        this.id = UUID.randomUUID();
+    @Override
+    public ResponseEntity<Void> addSession(LoginUser loginUser) {
+        if (Objects.isNull(id)) {
+            this.id = UUID.randomUUID();
+        }
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
+
+    @Override
+    public ResponseEntity<Void> deleteSession() {
+        this.id = null;
+        return ResponseEntity.noContent().build();
+    }
+
+    @Override
+    public ResponseEntity<GetUser> getSession() {
+        if (Objects.isNull(this.id))
+            return ResponseEntity.notFound().build();
+        return ResponseEntity.ok(
+                new GetUser()
+                        .alias("")
+                        .id(this.id.toString())
+                        .createdAt(OffsetDateTime.now())
+                        .lastName("")
+                        .firstName("")
+        );
+    }
+
 }
